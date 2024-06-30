b81a04211b7b91ed7036f9442dc230e157515b58
fitri-hy <noreply.orzpartners@gmail.com>
2024-06-30 15:34:39 +0700

    update

--- routes/PageRoute.js
+++ routes/PageRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+	const domain = req.get('host');
+    res.render('home', { domain });
 });
 
 module.exports = router;
