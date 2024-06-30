61eb559aaae5907e5f6725457064658887020828
fitri-hy <noreply.orzpartners@gmail.com>
2024-06-30 15:03:14 +0700

    update

--- routes/ApiRoute.js
+++ routes/ApiRoute.js
@@ -8,8 +8,11 @@ const upload = multer({ storage: storage });
 
 router.post('/generate/upload', upload.single('image'), (req, res) => {
     if (!req.file) {
+        console.log('No file uploaded.');
         return res.status(400).send('No file uploaded.');
     }
+
+    console.log('File uploaded:', req.file);
 
     Tesseract.recognize(
         req.file.buffer,
@@ -20,8 +23,10 @@ router.post('/generate/upload', upload.single('image'), (req, res) => {
     ).then(({ data: { text } }) => {
         res.json({ text });
     }).catch(err => {
+        console.error('Error processing image:', err);
         res.status(500).send('Error processing image');
     });
 });
+
 
 module.exports = router;
