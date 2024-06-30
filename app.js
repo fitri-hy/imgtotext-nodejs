a1f787c3aa26b843a229087b2851eedfec6fea7c
fitri-hy <noreply.orzpartners@gmail.com>
2024-06-30 14:58:34 +0700

    update

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const PageRoute = require('./routes/PageRoute');
 const app = express();
 const port = 3000;
 
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
