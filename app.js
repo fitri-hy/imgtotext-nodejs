6a00f88d4959d806a1ba220ca5810d2b76bedb8a
fitri-hy <noreply.orzpartners@gmail.com>
2024-06-30 14:42:01 +0700

    update

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use('/api', ApiRoute);
 app.use('/', PageRoute);
 
